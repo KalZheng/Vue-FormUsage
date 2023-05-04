a8659f6ed08c9efd20686c97e57f76cca37ebc20
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-04 15:57:57 +0800

    add fetch for values, such as check boxes, radio

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,30 +19,30 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest"/>
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="interest"/>
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest"/>
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value="video" v-model="how"/>
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how"/>
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value="others" v-model="how"/>
         <label for="how-other">Other</label>
       </div>
     </div>
@@ -57,6 +57,9 @@ export default {
     return {
       userName: '',
       userAge: null,
+      referrer: '',// drop down need a def
+      interest: [],//need to default array
+      how: null,
     };
   },
   methods: {
@@ -67,6 +70,10 @@ export default {
       console.log(this.userAge);
       console.log(31);
       console.log(this.$refs.ageInput.value);
+
+      console.log('referrer: '+ this.referrer);
+      console.log('interest: '+ this.interest);
+      console.log('how: '+ this.how);
     },
   },
 };
